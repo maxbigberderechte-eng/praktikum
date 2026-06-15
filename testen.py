6efb1833f36e7a748571a02584f7e41e40990644
Max <maxbigber.der.echte@gmail.com>
2026-06-15 13:59:04 +0200

    Aufgabe: Abstand Vertik

--- testen.py
+++ testen.py
@@ -9,21 +9,37 @@ zahlen_liste_klein = [1, -2]
 
 
 eingabe = [1,2,3,4,5,6]
+
+eingabe2 = [1,2,3,4,5,6,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1]
+nullen = [0,0]
 # Abstand von Zeile darüber [-1,-1,-1]
 
 
 
 
-def abstand(zwei_zahlen):
-    print("eingaeb",zwei_zahlen)
-    ergebnis = []
-    #zwei_zahlen_liste = int(zwei_zahlen.split(","))
-    #return (zwei_zahlen_liste[0] - zwei_zahlen_liste[1])
-    print("ergebnis", ergebnis)
-    return ergebnis
+def abstand(liste_von_zahlen):
+    ergebnis_liste = []
+    zahl1 = 0
+    zahl2 = 0
+
+    #print("einagbeänge", len(liste_von_zahlen))
+    for i in range(0, len(liste_von_zahlen)-1,2):
+        z1 = liste_von_zahlen[i]
+        z2 = liste_von_zahlen[i+1]
+
+        ergebnis = z1 - z2
+
+        ergebnis_liste.append(ergebnis)
+        print("i",i,"z1", z1,"z2",z2)
+
+    #for zahl in eingabe:
+        #zahl1 = zahl
+        #print(zahl1)
+
+
+    return ergebnis_liste
 
 abstand_von_eingabe = abstand(eingabe)
-
 print(abstand_von_eingabe)
 
 
